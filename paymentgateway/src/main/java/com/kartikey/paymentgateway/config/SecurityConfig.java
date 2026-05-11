ab410f9545037c00fe963e16817449a93dd896d1
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-12 00:14:03 +0530

    feat: added password encoder bean

--- paymentgateway/src/main/java/com/kartikey/paymentgateway/config/SecurityConfig.java
+++ paymentgateway/src/main/java/com/kartikey/paymentgateway/config/SecurityConfig.java
@@ -4,6 +4,8 @@ package com.kartikey.paymentgateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,6 +34,11 @@ public class SecurityConfig {
         ;
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
     
 }
