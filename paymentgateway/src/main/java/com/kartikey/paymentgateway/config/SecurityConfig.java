30e46e84ebd72780212e2497037fbd9f746e5528
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-11 23:46:42 +0530

    feat: create custom Security Filter Chain

---
+++ paymentgateway/src/main/java/com/kartikey/paymentgateway/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.kartikey.paymentgateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http
+            .csrf(csrf->csrf.disable())
+
+            .authorizeHttpRequests(auth->auth
+                .requestMatchers("/auth/**")
+                .permitAll()
+
+            .requestMatchers(
+                "/admin/**"
+            ).hasRole("ADMIN")
+            
+            .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+    
+}
