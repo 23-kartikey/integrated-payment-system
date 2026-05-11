4d3d64ebce53881b2bfa448244cc9bbede7bc285
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-11 23:05:56 +0530

    feat: create user entity

---
+++ paymentgateway/src/main/java/com/kartikey/paymentgateway/entity/User.java
@@ -0,0 +1,29 @@
+package com.kartikey.paymentgateway.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private long password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
